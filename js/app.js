4ebf7406a7575013ac9e9c84cb14a44f76e73e1e
Erin Shaw <erinshaw@Erins-MacBook-Air.local>
2015-05-24 14:36:51 -0700

    more functionality
    Press enter to add, prevent empty list items

--- js/app.js
+++ js/app.js
@@ -6,26 +6,41 @@ $('document').ready(function () {
 
 	function addItem() {
 		$(this).parent().css({'text-decoration':'line-through', 'color':'#C0C0C0'});
+		$(this).removeClass('btn-success');
 		$(this).parent().appendTo('#cart');
 	};
 
 	// When the #add-button is pressed
-	// append a list item with a checkbox and the input's text to #shelf
+	// append a list item with buttons and the input's text to #shelf
+	// and clear the #add-field for the next entry
 	$('#add-button').click(function() {
-		var item = $('#add-field').val();
 		var buttons = "<li><button class='btn btn-xs btn-success'><span class='glyphicon glyphicon-ok'></span></button>" + " " + "<button class='btn btn-xs btn-danger'><span class='glyphicon glyphicon-remove'></button>";
-		$('#shelf').append( buttons + " " + item);
-		$(item).val('');
+		$('#shelf').append( buttons + " " + $('#add-field').val());
+		$('#add-field').val('');
 	});
 
-	// When the checkbox in #shelf is pressed, add the list item text to #cart
-	// remove the list item from #shelf and append it to the last-child in #cart
+	// Prevent the creation of empty list items
+	$('#add-button').click(function(event) {
+    	if ($.trim($('#add-field').val()) === '') {
+    		event.preventDefault();
+	      alert('Add something to your list.');
+	      // return false;
+    }
+	});
+
+	// Pressing Enter has same functionality as pressing #add-button
+	$('#add-field').keyup(function(event) {
+		if(event.keyCode == 13) {
+			event.preventDefault();
+			$('#add-button').click();
+		};
+	});
 
 	$(document).on('click', '.btn-danger', deleteItem);
 
 	$(document).on('click', '.btn-success', addItem);
 }); // end of document.ready
 
-// Make it so that pressing enter will add an item
-// Prevent the creation of empty list items
-// Get rid of or lighten the buttons once they're in #cart
+// Fix the buttons in #cart
+// Even though the alert works, it still makes an empty list item
+// Change the alert to a dialog box on the page
